8473403a6addf67ed097e2b2d7d788a7f729b10e
Arthur Gusmão <arthurgusmao@id.uff.br>
2022-07-30 16:36:46 -0300

    mais um dia

--- TCC_v1.R
+++ TCC_v1.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
-pof_domicilio <- read_rds("/Users/apple/Documents/TCC/dados_rds/pof_domicilio.rds") # nolint
-pof_morador <- read_rds("/Users/apple/Documents/TCC/dados_rds/pof_morador.rds") # nolint
+pof_domicilio <- read_rds("/Users/apple/Documents/TCC/dados_rds/pof_domicilio_v1.rds") # nolint
+pof_morador <- read_rds("/Users/apple/Documents/TCC/dados_rds/pof_morador_v1.rds") # nolint
 
 pof_domicilio %>% glimpse()
 
@@ -37,12 +37,13 @@ pof_domicilio %>% write_rds("/Users/apple/Documents/TCC/dados_rds/pof_domicilio_
 
 
 pof_domicilio <- pof_domicilio %>%
-    select(V6199, NUM_DOM, id_dom) ##V6199 - Insegurança Alimentar
+    select(V6199, NUM_DOM, id_dom, TIPO_SITUACAO_REG, V0205, V0207, V0209,
+    V02111, V0212, V0215) ##V6199 - Insegurança Alimentar
 
-pof_morador %>%
-    filter(V0403 <=19) %>% ## V0403 - Idade
-    filter(V0306 != 01 & 17 & 18 & 19 ) %>% ## V0306 - Condição na UC - filho, neto e etc... # nolint
-    select(UF, ESTRATO_POF, PESO, PESO_FINAL,V0403,V0306, INSTRUCAO, COMPOSICAO,
-     PC_RENDA_DISP, PC_RENDA_MONET, PC_RENDA_NAO_MONET, id_dom, id_uc, id_pes) %>% glimpse()
-
-
+pof_morador <- pof_morador %>%
+    #filter(V0403 <=19) %>% ## V0403 - Idade
+    #filter(V0306 != 01 & 17 & 18 & 19 ) %>% ## V0306 - Condição na UC - filho, neto e etc... # nolint
+    select(UF, ESTRATO_POF, PESO, PESO_FINAL, V0403, V0306,
+    INSTRUCAO, COMPOSICAO, PC_RENDA_DISP, PC_RENDA_MONET,
+    PC_RENDA_NAO_MONET, id_dom, id_uc, id_pes,
+    V0404, V0405, ANOS_ESTUDO)
